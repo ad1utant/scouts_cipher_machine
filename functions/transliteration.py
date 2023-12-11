ed22a71fac29a5879b11a532792b291b9a88b61a
adiutant <mikolaj@marasek.eu>
2023-12-11 12:03:16 +0100

    add transliteration module

---
+++ functions/transliteration.py
@@ -0,0 +1,10 @@
+def Transliteration(inp):
+    inp.replace('ą','a')
+    inp.replace('ć','c')
+    inp.replace('ę','e')
+    inp.replace('ł','l')
+    inp.replace('ó','o')
+    inp.replace('ś','s')
+    inp.replace('ż','z')
+    inp.replace('ź','z')
+    return inp
--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-if __name__ == '__main__':
-    dict = {'g': 'a', 'd': 'e', 'r': 'y', 'p': 'o', 'l': 'u', 'k': 'i', 'a': 'g', 'e': 'd', 'y': 'r', 'o': 'p',
-            'u': 'l', 'i': 'k', 'ó': 'p', 'ą': 'g', 'ę': 'd', 'ł': 'u', 'ś': 's', 'ń': 'n', 'ź': 'z', 'ć': 'c',
-            'ż': 'z'}
-    console = input()
-    temp = True
-    newConsole = ""
-    for i in console:
-        for j in dict:
-            if i == j:
-                temp = False
-                newConsole += dict[j]
-                break
-        if temp:
-            newConsole += i
-        temp = True
+from functions.transliteration import Transliteration
 
-    print(newConsole)
+
+if __name__ == '__main__':
+    inp = Transliteration(input())
+    print(inp)
