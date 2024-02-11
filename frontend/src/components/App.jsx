c732a298c231cd437c52c119de5d11cd53f00b32
adiutant <mikolaj@marasek.eu>
2024-02-12 00:58:55 +0100

    basic functionality

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,10 +1,35 @@
-function App() {
+import {useRef} from "react";
 
+function App() {
+    const formRef = useRef();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const promptValue =  formRef.current.elements.promptInput.value;
+        const keyValue = formRef.current.elements.keyInput.value;
+        async function fetchRequest(){
+            try {
+                const response = await fetch(`http://127.0.0.1:8000/${keyValue}+${promptValue}`);
+                const newConsole = await response.json();
+                const {newPrompt} = newConsole;
+                console.log(newPrompt)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        fetchRequest()
+
+
+    }
   return (
       <div>
-          scouts-cipher-machine
+          <form ref={formRef}>
+              <input type={"text"} name={'promptInput'} placeholder={'prompt'}/>
+              <input type={"text"} name={'keyInput'} placeholder={'key'}/>
+              <button type={"submit"} onClick={handleSubmit}>submit</button>
+          </form>
       </div>
   )
 }
 
-export default App
+export default App;
